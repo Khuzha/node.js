930035504a37c31519e6dc78095e843d2bdb4f7f
Khuzha <t.sardorkhuja@gmail.com>
2018-08-11 02:20:21 +0500

    Some first files

---
+++ 10.08/index.js
@@ -0,0 +1,7 @@
+//min(2, 5) == 2
+//min(3, -1) == -1
+//min(1, 1) == 1
+
+function minNum (a, b) {
+	console.log (a > b) ? a : b;
+}
---
+++ 10.08/index3.js
@@ -0,0 +1,11 @@
+function zakT () {
+	var a = 10;
+}
+zakT();
+//console.log a;
+
+var tashZ() => {
+	let b = 15;
+}
+tashZ();
+console.log b;
