4daffba2bec7682e33ba60048e998987ddee78d1
Khuzha <t.sardorkhuja@gmail.com>
2018-08-12 00:14:14 +0500

    ...

--- 11.09/2index.js
+++ 11.09/2index.js
@@ -1,3 +1,8 @@
+//Мораль файла: только функции, объявленные в формате
+//var sayHi = function(){} можно вызывать в коде 
+//до того места, где они записаны
+//Остальные будут крашиться
+
 //sayHi(); - НЕ работает
 
 var sayHi = function(){
@@ -20,3 +25,16 @@ function sagenHallo() {
 var aytSalom = () => {
 	console.log('Salom');
 }
+
+/*
+ПРОСТО ПРИМЕР КОДА (использование Function Expression с тернарным оператором ?):
+
+var age = prompt('Сколько вам лет?');
+
+var sayHi = (age >= 18) ?
+  function() { alert('Прошу Вас!');  } :
+  function() { alert('До 18 нельзя'); };
+
+sayHi();
+
+ */
