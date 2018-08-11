f1deec8075f49aa8158317a75a6e575f1c8771c2
Khuzha <t.sardorkhuja@gmail.com>
2018-08-12 00:12:12 +0500

    ...

---
+++ 11.09/2index.js
@@ -0,0 +1,22 @@
+//sayHi(); - НЕ работает
+
+var sayHi = function(){
+	console.log('Hi');
+}
+
+//-----------------------------
+
+// sagenHallo(); - работает
+
+function sagenHallo() {
+	console.log('Hallo');
+}
+
+//-----------------------------
+
+
+//aytSalom(); - не работает
+
+var aytSalom = () => {
+	console.log('Salom');
+}
