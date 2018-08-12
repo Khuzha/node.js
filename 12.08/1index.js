9390f09cf0bf4b3eaf4ccd38c551c5ecef2e1c22
Khuzha <t.sardorkhuja@gmail.com>
2018-08-12 21:39:11 +0500

    Numbers and Objects

---
+++ 12.08/1index.js
@@ -0,0 +1,9 @@
+//Объекты: https://learn.javascript.ru/object
+
+var firstObj = {};
+
+firstObj['name'] = 'Вася';
+firstObj['surname'] = 'Петров';
+firstObj['name'] = "Сергей";
+delete firstObj['name'];
+
