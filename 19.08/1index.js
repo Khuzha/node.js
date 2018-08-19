7aa45bf97c8225493fce83a754612b67279cac32
Khuzha <t.sardorkhuja@gmail.com>
2018-08-19 11:41:44 +0500

    Random.org

---
+++ 19.08/1index.js
@@ -0,0 +1,14 @@
+//Рандомный победитель 10-го айфона
+
+var names = 'Сардор, Вася, Петя, Коля, Боря'; //сюда через запятую вводим имена, ибо prompt() в терминале не работает
+var arrayOfNames = names.split(','); //переводит строку в ячейки массива, разделяя их при каждой запятой
+console.log('Все участники конкурса: ' + arrayOfNames);
+
+
+// Возвращает случайное целое число между min (включительно) и max (не включая max)
+// Использование метода Math.round() даст вам неравномерное распределение!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log('Айфон Х выигрывает... барабанная дробь...:' + arrayOfNames[getRandomInt(0, arrayOfNames.length)]);
