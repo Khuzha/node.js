c532647d99f0145fa5e82f0aac7eae3758b49398
Khuzha <t.sardorkhuja@gmail.com>
2018-08-19 17:55:47 +0500

    ...

--- 19.08/2index.js
+++ 19.08/2index.js
@@ -1,9 +1,30 @@
 const prompt = require('prompt-sync')();
 
-let name = [];
+let names = [];
+var i = 0
 
-for (let i = 0; name[i] !== undefined; i++) {
-	name[i] = prompt('Вводите имена');
+/*
+for (names[i] = null; names[i] !== 'Спасибо. Ввод имен завершен.'; i++) {
+	names[i] = prompt('Вводите имена: ', 'Спасибо. Ввод имен завершен.');
+} 
+*/
+
+//while (true) {
+for (; true; i++) {
+	if (i == 0) {
+		names[i] = prompt('Введите первое имя. Для завершения ввода оставьте строку пустой и просто нажмите Enter: ', 'Спасибо. Ввод имен завершен.');
+		if (names[i] === 'Спасибо. Ввод имен завершен.') {
+			console.log(names[i]);
+			break;
+		} 
+	} else {
+		names[i] = prompt('Вводите имена: ', 'Спасибо. Ввод имен завершен.');
+		if (names[i] === 'Спасибо. Ввод имен завершен.') {
+			console.log(names[i]);
+			break;
+		} 
+	}
 }
 
-console.log(name);
+
+//console.log(names);
---
+++ 19.08/3index.js
@@ -0,0 +1,6 @@
+for (var a = 1; a > 0; a++) {
+	if(a > 10) {
+		break;
+	}
+	console.log(a);
+}
