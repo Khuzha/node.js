c82bdb75335607112dcd0727efc8c310876feb43
Khuzha <t.sardorkhuja@gmail.com>
2018-08-19 18:55:52 +0500

    ...

--- 19.08/2index.js
+++ 19.08/2index.js
@@ -1,30 +1,24 @@
 const prompt = require('prompt-sync')();
 
 let names = [];
-var i = 0
-
-/*
-for (names[i] = null; names[i] !== 'Спасибо. Ввод имен завершен.'; i++) {
-	names[i] = prompt('Вводите имена: ', 'Спасибо. Ввод имен завершен.');
-} 
-*/
+var i = 0;
 
 //while (true) {
 for (; true; i++) {
 	if (i == 0) {
 		names[i] = prompt('Введите первое имя. Для завершения ввода оставьте строку пустой и просто нажмите Enter: ', 'Спасибо. Ввод имен завершен.');
-		if (names[i] === 'Спасибо. Ввод имен завершен.') {
+		if (names[i] === 'Спасибо. Ввод имен завершен.' || names[i] === null || names[i] === '') {
 			console.log(names[i]);
 			break;
 		} 
 	} else {
 		names[i] = prompt('Вводите имена: ', 'Спасибо. Ввод имен завершен.');
-		if (names[i] === 'Спасибо. Ввод имен завершен.') {
+		if (names[i] === 'Спасибо. Ввод имен завершен.' || names[i] === null || names[i] === '') {
+			names.pop();
 			console.log(names[i]);
 			break;
 		} 
 	}
 }
 
-
-//console.log(names);
+console.log(names);
