a996fc5337f4c5c11baa189616e219e7f1053bd1
Khuzha <t.sardorkhuja@gmail.com>
2018-08-20 08:58:26 +0500

    ...

--- 19.08/4index.js
+++ 19.08/4index.js
@@ -1,8 +1,9 @@
 //добавить класс в строку
+const prompt = require('prompt-sync')();
 var obj = {
   className: 'open menu'
 }
-
+/*
 var arr = [];
 var prop = obj.className;
 var arr = prop.split(' ');
@@ -11,3 +12,22 @@ arr.splice(arr.length, 0, 'lolkek');
 //console.log(arr);
 var result = arr.join(' ');
 console.log(result);
+*/	
+
+var addClass = (nc) => {
+	var prop = obj.className;
+	var arr = prop.split(' ');
+	var addon = '';
+	for (var key in arr) {
+		if(nc !== arr[key]) {
+			ke
+		}
+	}
+	ln = arr.length + 1;
+	arr[ln] = addon;
+	var result = arr.join(' ');
+	console.log(result);
+}
+
+nc = prompt('Введите новый элемент', '');
+addClass(nc)
